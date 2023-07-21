5f999c26b9df048239454679ad9662533a14b31e
ned1313 <ned.bellavance@gmail.com>
2023-07-21 08:28:40 -0400

    provider plugins

---
+++ provider_plugins/azure_example.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {
+
+  }
+}
+
+provider "azurerm" {
+  alias = "security_sub"
+  features {
+
+  }
+}
---
+++ provider_plugins/syntax.tf
@@ -0,0 +1,17 @@
+terraform {
+  required_providers {
+    NAME = {
+      source  = "SOURCE_ADDRESS"
+      version = "VERSION_CONSTRAINT"
+    }
+  }
+}
+
+provider "NAME" {
+  # Provider specific arguments
+}
+
+provider "NAME" {
+  alias = "ALIAS_NAME"
+  # Provider specific arguments
+}
